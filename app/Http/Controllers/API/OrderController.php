29079d557c627d7ad8813a12d886abaa7a74d3f9
Ahmad Hussain <ahmadawmh@gmail.com>
2020-11-15 15:57:06 +0200

    wip

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -9,7 +9,7 @@ class OrderController
 {
     public function index()
     {
-        $orders = auth()->user()->orders->paginate(20);
+        $orders = auth()->user()->orders()->paginate(20);
 
         return OrderResource::collection($orders);
     }
