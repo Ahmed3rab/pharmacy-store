a22a1d5495e21187a1fbfb50a324f6696137f9e3
Ahmed3rab <ahmedmohamedarab@gmail.com>
2020-11-30 14:14:18 +0200

    Rearrange web routes file

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,32 @@ use App\Http\Controllers\CP\OrdersController;
 use App\Http\Controllers\CP\ProductsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('cp', [HomeController::class, 'show'])->name('cp');
-
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('auth.login');
-})->name('auth.login');
+})->name('login');
 
 Route::post('auth/authenticate', [LoginController::class, 'authenticate'])->name('auth.authenticate');
+Route::redirect('/', 'cp');
+Route::group(['prefix' => 'cp', 'middleware' => 'auth'], function () {
+    Route::get('/', [HomeController::class, 'show'])->name('cp');
 
-# Orders
-Route::get('cp/orders', [OrdersController::class, 'index'])->name('orders.index');
-Route::get('cp/orders/{order}', [OrdersController::class, 'show'])->name('orders.show');
-Route::patch('cp/orders/{order}', [CompleteOrderController::class, 'store'])->name('orders.complete.store');
+    # Orders
+    Route::get('orders', [OrdersController::class, 'index'])->name('orders.index');
+    Route::get('orders/{order}', [OrdersController::class, 'show'])->name('orders.show');
+    Route::patch('orders/{order}', [CompleteOrderController::class, 'store'])->name('orders.complete.store');
 
-# Products
-Route::get('cp/products', [ProductsController::class, 'index'])->name('products.index');
-Route::get('cp/products/create', [ProductsController::class, 'create'])->name('products.create');
-Route::post('cp/products', [ProductsController::class, 'store'])->name('products.store');
-Route::get('cp/products/{product}/edit', [ProductsController::class, 'edit'])->name('products.edit');
-Route::put('cp/products/{product}', [ProductsController::class, 'update'])->name('products.update');
-Route::delete('cp/products/{product}', [ProductsController::class, 'destroy'])->name('products.delete');
+    # Products
+    Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+    Route::get('products/create', [ProductsController::class, 'create'])->name('products.create');
+    Route::post('products', [ProductsController::class, 'store'])->name('products.store');
+    Route::get('products/{product}/edit', [ProductsController::class, 'edit'])->name('products.edit');
+    Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+    Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.delete');
 
-# Categories
-Route::get('cp/categories', [CategoriesController::class, 'index'])->name('categories.index');
-Route::get('cp/categories/create', [CategoriesController::class, 'create'])->name('categories.create');
-Route::post('cp/categories', [CategoriesController::class, 'store'])->name('categories.store');
-Route::get('cp/categories/{category}/edit', [CategoriesController::class, 'edit'])->name('categories.edit');
-Route::patch('cp/categories/{category}', [CategoriesController::class, 'update'])->name('categories.update');
+    # Categories
+    Route::get('categories', [CategoriesController::class, 'index'])->name('categories.index');
+    Route::get('categories/create', [CategoriesController::class, 'create'])->name('categories.create');
+    Route::post('categories', [CategoriesController::class, 'store'])->name('categories.store');
+    Route::get('categories/{category}/edit', [CategoriesController::class, 'edit'])->name('categories.edit');
+    Route::patch('categories/{category}', [CategoriesController::class, 'update'])->name('categories.update');
+});
