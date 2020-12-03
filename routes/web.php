8240605a04710acf8c12a057c13d17879f565a25
Khawlah Elshah <khawlah_t@yahoo.com>
2020-12-03 14:32:53 +0200

    fix login route name

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('auth.login');
-})->name('login');
+})->name('auth.login');
 
 Route::post('auth/authenticate', [LoginController::class, 'authenticate'])->name('auth.authenticate');
 Route::redirect('/', 'cp');
