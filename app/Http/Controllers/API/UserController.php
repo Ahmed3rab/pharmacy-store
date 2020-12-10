548617db5f9bce1312143b4b9a74106e05420ac3
Khawlah Elshah <khawlah_t@yahoo.com>
2020-12-10 16:10:47 +0200

    add logout form

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rule; 
 
 class UserController extends Controller
 {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,10 +103,9 @@
                         </svg>
                     </button>
 
-                    <!-- Profile dropdown -->
-                    <div class="ml-3 relative">
+                    <div x-data="{ open: false }" class="ml-3 relative">
                         <div>
-                            <button
+                            <button @click="open = !open"
                                 class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:shadow-outline"
                                 id="user-menu" aria-label="User menu" aria-haspopup="true">
                                 <img class="h-8 w-8 rounded-full"
@@ -114,17 +113,14 @@
                                     alt="">
                             </button>
                         </div>
-                        <!--
-                          Profile dropdown panel, show/hide based on dropdown state.
 
-                          Entering: "transition ease-out duration-100"
-                            From: "transform opacity-0 scale-95"
-                            To: "transform opacity-100 scale-100"
-                          Leaving: "transition ease-in duration-75"
-                            From: "transform opacity-100 scale-100"
-                            To: "transform opacity-0 scale-95"
-                        -->
-                        <div class="hidden origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+                        <div x-show="open == true" x-transition:enter="transition ease-out duration-100"
+                            x-transition:enter-start="transform opacity-0 scale-95"
+                            x-transition:enter-end="transform opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-75"
+                            x-transition:leave-start="transform opacity-100 scale-100"
+                            x-transition:leave-end="transform opacity-0 scale-95"
+                            class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
                             <div class="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical"
                                 aria-labelledby="user-menu">
                                 <a href="#"
@@ -135,9 +131,14 @@
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
                                     role="menuitem">Settings</a>
 
-                                <a href="#"
+                                <a onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
                                     role="menuitem">Sign out</a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </div>
                         </div>
                     </div>
