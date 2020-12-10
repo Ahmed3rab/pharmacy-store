9ba7b0a27e9649b88e62ec8dc599bf4c3d8d28c9
Khawlah Elshah <khawlah_t@yahoo.com>
2020-12-10 12:06:48 +0200

    add phone_number to user account update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,11 +9,13 @@ class UserController extends Controller
     public function update()
     {
         $data = request()->validate([
-            'name' => 'required|string',
+            'name'         => 'required|string',
+            'phone_number' => 'required|numeric|digits_between:9,10|unique:users,phone_number',
         ]);
 
         auth()->user()->update([
-            'name' => $data['name'],
+            'name'         => $data['name'],
+            'phone_number' => $data['phone_number'],
         ]);
 
         return response()->json([], 204);
